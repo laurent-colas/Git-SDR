3c4f15ee23d12fb669b29c925d2319f7278263c6
Neuromap <joel.gaudreault@usherbrooke.ca>
2020-10-31 17:11:28 -0400

    First commit of the .bin file generator for NM-382

---
+++ GNURadio/tests/BinGenerator.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 30 12:16:51 2020
+
+@author: neuromap
+"""
+import numpy as np
+
+class Words:
+    def __init__(self):
+        self.byte = 0
+        
+    def toBinary(self):
+        binaryList = []
+        # Transfer the data in binary array
+        binaryList = [int(i) for i in list(format(self.byte, '08b'))]
+        # Add the start and stop bit
+        binaryList.insert(0,0)
+        binaryList.append(1)
+        
+        return binaryList
+
+class Preambule(Words):
+    def __init__(self):
+        self.byte = 170 # Corresponds to 0xAA
+        
+class StartFrame(Words):
+    def __init__(self):
+        self.byte = 114 # Corresponds to 0x72
+        
+class Type(Words):
+    def __init__(self):
+        self.byte = 0 #
+        
+class Address(Words):
+    pass
+        
+class Sample(Words):
+    pass
+
+class Frame:
+    def __init__(self):
+        Pre = Preambule()
+        Sta = StartFrame()
+        Typ = Type()
+        self.data = [Pre, Pre, Sta, Typ]
+        
+    def Add(self, Address, Sample):
+        pass
+    
+    def toStream(self):
+        stream =[]
+        # Fetch all the words in the frame with the preambule
+        for word in self.data:
+            stream.extend(word.toBinary())
+        
+        # Add the end of frame
+        stream.extend([1,1,1,1,1,1,1,1,1,1])
+        return stream
+    
+    def toFile(self, filename, mult):
+        stream = self.toStream()
+        # Duplicate each element in stream mult times
+        if mult != 0:
+            multStream = []
+            for element in stream:
+                for i in range(mult):
+                    multStream.append(element)
+            newFile = open(filename + ".bin", "wb")
+            newFile.write(bytes(multStream))
+        else:
+            newFile = open(filename + ".bin", "wb")
+            newFile.write(bytes(stream))
+            
+# Main test
+test = Frame()
+test.toFile("filename", 0)
